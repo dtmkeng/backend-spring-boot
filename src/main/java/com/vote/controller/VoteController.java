cf8f83989769d45adaf8111a8c9f185177c6aadf
dtmkeng <kang081@hotmail.com>
2019-09-13 00:43:38 +0700

    req body

---
+++ src/main/java/com/vote/controller/VoteController.java
@@ -0,0 +1,33 @@
+
+package com.vote.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.vote.repository.*;
+import com.vote.entity.*;
+import com.vote.model.*;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class VoteController {
+
+    @Autowired
+    private final VoteRepository  voteRepository;
+
+    public VoteController(VoteRepository  voteRepository) {
+            this.voteRepository = voteRepository;
+    }
+    @PostMapping("/request")
+    public BodyVote index(@RequestBody BodyVote bodyVote) {
+        System.out.println(bodyVote.getUsername());
+        System.out.println(bodyVote.getPassword());
+		return bodyVote;
+	}
+}
---
+++ src/main/java/com/vote/model/BodyVote.java
@@ -0,0 +1,8 @@
+package com.vote.model;
+import lombok.*;
+
+@Data
+public  class BodyVote {
+    private String username;
+    private String password;
+}
